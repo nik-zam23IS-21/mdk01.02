d90d2ee7c7c307d039cd9b7b945c6b8541d34a2c
nik-zam23IS-21 <119483966+nik-zam23IS-21@users.noreply.github.com>
2023-06-02 09:07:09 +0300

    Add files via upload

---
+++ Math test/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+        // These integer variables store the numbers 
+        // for the addition problem. 
+        int addend1;
+        int addend2;
+
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
+        // This integer variable keeps track of the 
+        // remaining time.
+        int timeLeft;
+
+
+        /// <summary>
+        /// Start the quiz by filling in all of the problems
+        /// and starting the timer.
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            // Start the timer.
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+            if(timeLeft==30)
+            {
+                timeLabel.BackColor= Color.White;
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        /// <summary>
+        /// Check the answers to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("Все ответы верны!",
+                                "Поздравляю!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() returns false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " секунды";
+                if (timeLeft == 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Время вышло!";
+                MessageBox.Show("Вам не хватило времени.", "Попробуйте еще раз!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void новыйТестToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+    }
+}
---
+++ Pass Checker/ClassTests/PasswordCheckerTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+
+        [TestMethod()]
+        public void Chek_8Symbols_ReturnsTrue()
+        {
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Chek_4Symbols_ReturnsFalse()
+        {
+            string password = "Aq1$";
+            
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void Chek_30Symbols_ReturnsFalse()
+        {
+            string password = "ASDqwe123$ASDqwe123$ASDqwe123$";
+            bool expected = false;
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Chek_PasswordWithDigits_ReturnsTrue()
+        {
+            string password = "ASDqwe1$";
+            bool expected = true;
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Chek_PasswordWithLowerSymbols_ReturnsTrue()
+        {
+            string password = "ASDq123$";
+            bool expected = true;
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        [TestMethod()]
+        public void Chek_30Symbols_ReturnsFalse()
+        {
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}

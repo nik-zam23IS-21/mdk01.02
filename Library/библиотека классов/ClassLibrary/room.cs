93b7645d1d6c3b07ddccae479bd8ac16cd144636
nik-zam23IS-21 <119483966+nik-zam23IS-21@users.noreply.github.com>
2023-06-02 09:04:17 +0300

    Add files via upload

---
+++ Library/библиотека классов/ClassLibrary/room.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double RoomLength
+        {
+            get { return roomLength; } 
+            set { roomLength = value; }
+        }
+
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+
+        public double RoomPerimeter()
+        {
+            return 2 * (RoomLength + RoomWidth);
+        }
+
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+
+        public double PersonaArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
